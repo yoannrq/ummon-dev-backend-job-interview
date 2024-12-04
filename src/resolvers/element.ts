bc6a787f0e0f21d2858b0342c397a0016b2d29e9
Yoann Rouquié <120712901+yoannrq@users.noreply.github.com>
2024-12-04 10:38:52 +0100

    feat: Add part relation in element resolver

--- src/resolvers/element.ts
+++ src/resolvers/element.ts
@@ -1,11 +1,18 @@
 import { AppDataSource } from "../data-source";
 import { Element } from "../entities/Element";
 import { Color } from "../entities/Color";
+import { Part } from "../entities/Part";
 
 export const ElementResolver = {
   color: async (element: Element, _variables: {}) => {
     return await AppDataSource.manager.findOneBy(Color, {
       id: element.colorId,
+    });
+  },
+
+  part: async (element: Element, _variables: {}) => {
+    return await AppDataSource.manager.findOneBy(Part, {
+      partNum: element.partNum,
     });
   },
 };
